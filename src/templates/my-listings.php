36f233e6463f18a05da60c61d2daa6d5d11852bc
Bryan Baird <bryan@kerigan.com>
2019-02-19 09:56:15 -0500

    added assets and css

---
+++ src/templates/my-listings.php
@@ -0,0 +1,15 @@
+<?php
+
+use Includes\Modules\KMARealtor;
+
+?>
+<div id="kmarealtor" >
+    <div class="kma-header">
+        <div class="header py-4 bg-dark text-light">
+            <h1>My Listings</h1>
+        </div>
+    </div>
+    <div class="content">
+
+    </div>
+</div>
